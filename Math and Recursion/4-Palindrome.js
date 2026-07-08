946edd140152c5cc8c63f386622ec87f71b0184e
prashanksingh93-source <prashanksingh93@gmail.com>
2026-07-08 20:58:54 +0530

    dsa

---
+++ Math and Recursion/4-Palindrome.js
@@ -0,0 +1,13 @@
+let num=12321;
+let orginal=num;
+let rev=0;
+for(let i=0;num>0;i++){
+   let lastnum=num%10;
+   rev=rev*10+lastnum;
+   num=Math.floor(num/10)
+
+   }
+ console.log(rev)
+ console.log(orginal);
+ console.log(rev==orginal)
+ 
