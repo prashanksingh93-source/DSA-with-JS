8f053a5c585511262f69ecd6a50f7e4b55e6eb71
prashanksingh93-source <prashanksingh93@gmail.com>
2026-08-21 19:53:49 +0530

    left rotation in array

---
+++ Arrays & Strings/RotateArray.js
@@ -0,0 +1,19 @@
+function reverse(arr,start,end){
+   while(start<end){
+       temp=arr[start];
+       arr[start]=arr[end];
+       arr[end]=temp;
+     start++;
+     end--;
+   }
+}
+
+let arr=[1,2,3,4,5,6];
+let k=2;
+ k=k%arr.length //for repeatation
+
+ //arr rotate left 
+ reverse(arr,0,k-1);
+ reverse(arr,k,arr.length-1);
+ reverse(arr,0,arr.length-1);
+ console.log(arr);
