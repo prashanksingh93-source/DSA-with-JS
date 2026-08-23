45df39d632eb2b92d981b7e44716a8965c4fcafa
prashanksingh93-source <prashanksingh93@gmail.com>
2026-08-23 13:52:01 +0530

    anagram check

---
+++ Strings/AnagramCheck.js
@@ -0,0 +1,19 @@
+function anagram(s,t){
+    s=s.toLowerCase().replace(/\s+/g,"");
+    t=t.toLowerCase().replace(/\s+/g,"");
+
+    let freq={};
+
+    if(s.length!=t.length) return false;
+
+    for(let key of s){
+        freq[key]=(freq[key]||0)+1;
+        }
+    for(let key of t){
+        if(!freq[key]) return false;
+        freq[key]--;
+    }
+    return true;
+}
+
+console.log(anagram('annu','nuna'));
