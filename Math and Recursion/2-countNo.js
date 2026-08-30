56ae9c6ed82ea35769809b8743680557e9760aab
prashanksingh93-source <prashanksingh93@gmail.com>
2026-08-30 09:24:39 +0530

    Update 2-countNo.js

--- Math and Recursion/2-countNo.js
+++ Math and Recursion/2-countNo.js
@@ -1,8 +1,9 @@
 //Count the Number of Digits in a Number Using a for Loop
 let num=1413435;
 let count=0
-for(;num>0;count++){
+while(num>0){
      num=Math.floor(num/10)
+     count++
 }
 
 console.log(count)
