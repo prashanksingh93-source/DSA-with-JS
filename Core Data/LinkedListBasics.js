c5b0bb676c3e129d0d30bb4edaeea5f3f40a842b
prashanksingh93-source <prashanksingh93@gmail.com>
2026-08-31 19:23:30 +0530

    Update LinkedListBasics.js

--- Core Data/LinkedListBasics.js
+++ Core Data/LinkedListBasics.js
@@ -1,23 +1,78 @@
-class node{
+// class node{
+//     constructor(data){
+//         this.data=data;
+//         this.next=null;
+//     }
+// }
+
+// let node1=new node(10);
+// let node2=new node(20);
+// let node3=new node(30);
+// let node4=new node(40);
+
+// node1.next=node2;
+// node2.next=node3;
+// node3.next=node4;
+
+// let head=node1;
+// let connect=head;
+
+// while(connect!=null){
+//   console.log(connect.data);
+//   connect=connect.next;
+// }
+
+
+
+
+
+//linkdin basic
+class Node{
     constructor(data){
-        this.data=data;
-        this.next=null;
+        this.data = data;
+        this.next = null;
+    }
+}
+class LinkedList{
+    constructor(){
+        this.head = null;
+    }
+
+    append(data){
+        let newNode = new Node(data);
+
+        if(!this.head){
+            this.head = newNode;
+            return;
+        }
+
+        let current = this.head;
+
+        while(current.next){
+            current = current.next;
+        }
+
+        current.next = newNode;
+    }
+
+    print(){
+        let current = this.head;
+        let result = "";
+
+        while(current){
+            result += current.data + "->";
+
+            current = current.next;
+        }
+
+        console.log(result + "null")
     }
 }
 
-let node1=new node(10);
-let node2=new node(20);
-let node3=new node(30);
-let node4=new node(40);
+let list = new LinkedList();
 
-node1.next=node2;
-node2.next=node3;
-node3.next=node4;
+list.append(10);
+list.append(20);
+list.append(30);
 
-let head=node1;
-let connect=head;
-
-while(connect!=null){
-  console.log(connect.data);
-  connect=connect.next;
-}
+list.print();
